04f7b1c7918f02d933ec92e880e3e67fe36ecf18
Daniel Creasy <dcreasy.pf@gmail.com>
2019-01-19 22:58:37 -0800

    adD SOMe map things trying to make a map grid

--- mapFetch.js
+++ mapFetch.js
@@ -1,3 +1,13 @@
-fetch('/map.json', (json) => {
-    console.log('json', json)
+console.log("hello world")
+let boxes;
+fetch('/map.json')
+    .then(res => res.json())
+    .then((json) => boxes = json)
+
+let body = document.querySelector('.realbody');
+
+boxes.forEach((obj) => {
+    const newDiv = document.createElement('div');
+    newDiv.innerHTML = `<img src='${imagePath}' />${location.toString()}`
+    body.appendChild(newDiv)
 })
